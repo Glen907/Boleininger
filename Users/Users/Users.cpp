74ea56ed2b7343f2ba71653f664c05ee2a4a022d
Gleb <144655536+Glen907@users.noreply.github.com>
2024-05-08 12:10:16 +0300

    Add files via upload

---
+++ Users/Users/Users.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    string line = "Users.txt";
+
+    ofstream fout;
+
+    fout.open(line);
+
+    if (!fout.is_open())
+    {
+        cout << "Файл не найден!\n";
+    }
+    else
+    {
+        fout << "1  Анна  25  Москва\n2  Иван  30  Санкт-Петербург\n3  Мария  28  Киев\n";
+        fout << "4  Витя  20  Ростов\n5  Катя  56  Луганск";
+    }
+    fout.close();
+}
